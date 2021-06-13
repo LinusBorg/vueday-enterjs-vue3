98389b00c433c28a869b103a31a4b62498d6045c
Thorsten Luenborg <t.luenborg@googlemail.com>
2021-06-13 12:27:03 +0200

    externalize windi config so extension can pick it up

--- setup/windicss.ts
+++ setup/windicss.ts
@@ -1,11 +1,3 @@
 import { defineWindiSetup } from "@slidev/types"
-export default defineWindiSetup(() => ({
-  theme: {
-    extend: {
-      colors: {
-        vgreen: '#41B883',
-        vblue: '#34495E',
-      },
-    }
-  }
-}))
+import windiConfig from '../windi.config.js'
+export default defineWindiSetup(() => windiConfig)
---
+++ windi.config.js
@@ -0,0 +1,10 @@
+export default {
+  theme: {
+    extend: {
+      colors: {
+        vgreen: "#41B883",
+        vblue: "#34495E",
+      },
+    },
+  },
+};
